2b4560efc7b7ae9584e38383ddb7675c06caa8c2
torii_y <torii_y@worksap.co.jp>
2015-08-13 19:49:22 +0900

    fix LICENSE and add docs

--- src/Transition.elm
+++ src/Transition.elm
@@ -1,25 +1,41 @@
 module Transition (Action, init, Model, update, start, reverse, toggle) where
+{-| A simple transition library for Elm.
+# Definition
+@docs Action, Model
+# Architecture
+@docs init, update
+# Actions
+@docs start, reverse, toggle
+-}
 
 import String exposing (..)
 import Signal exposing (Address)
 import Time exposing (..)
 import Effects exposing (..)
 
+{-| All actions should be passed to the `update` function following the Elm Architecture.
+-}
 type Action
   = Start Time
   | Reverse Time
   | Toggle Time
   | Next Time Time Time Bool
 
+{-| The ratio is the value of elapsed time per duration which varies from 0 to 1.
+-}
 type alias Model =
   { ratio : Float
   }
 
+{-| The initial ratio is 0.0.
+-}
 init : Model
 init =
   { ratio = 0.0
   }
 
+{-| All actions should be passed to this function following the Elm Architecture.
+-}
 update : Action -> Model -> (Model, Effects Action)
 update action model =
   case action of
@@ -64,11 +80,23 @@ update action model =
       in
         (newModel, effects)
 
+{-| Start changing the `ratio` from 0 to 1.
+The first argument is the duration between start and end.
+Interruption is not allowed during the transition.
+-}
 start : Float -> Action
 start = Start
 
+{-| Start changing the `ratio` from 1 to 0.
+The first argument is the duration between start and end.
+Interruption is not allowed during the transition.
+-}
 reverse : Float -> Action
 reverse = Reverse
 
+{-| Start changing the `ratio` from 0 to 1 when current ratio is 0, from 1 to 0 when current ratio is 1.
+The first argument is the duration between start and end.
+Interruption is not allowed during the transition.
+-}
 toggle : Float -> Action
 toggle = Toggle
